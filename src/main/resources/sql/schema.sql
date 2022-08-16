4c9310c07daff4cd9344bcde287232af06c2ec8a
mynameisdaun <daun9870.jung@gmail.com>
2022-08-17 08:45:27 +0900

    <refactor> table column name 변경

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,17 +1,17 @@
-/* 22.08.16 회원 테이블 */
+/* 22.08.17 회원 테이블 */
 CREATE TABLE member
 (
-    member_id          int auto_increment  not null comment '회원 구분자(seq)',
-    member_email       varchar(100) unique not null comment '회원 email',
-    member_password    varchar(100) comment '회원 비밀번호 추후 수정 22.08.16',
-    member_nickname    varchar(20)         not null comment '회원 닉네임',
-    member_social_type char(1)             not null comment '회원가입 시 사용한 Social Portal, W: 자체회원, K: 카카오 / G: 구글 / N : 네이버',
-    created_at         datetime            not null default current_timestamp comment '데이터 생성일시',
-    updated_at         datetime            not null default current_timestamp comment '데이터 수정일시',
-    primary key (member_id)
+    id          int auto_increment  not null comment '회원 구분자(seq)',
+    email       varchar(100) unique not null comment '회원 email',
+    password    varchar(100) comment '회원 비밀번호 추후 수정 22.08.16',
+    nickname    varchar(20)         not null comment '회원 닉네임',
+    social_type char(1)             not null comment '회원가입 시 사용한 Social Portal, W: 자체회원, K: 카카오 / G: 구글 / N : 네이버',
+    created_at  datetime            not null default current_timestamp comment '데이터 생성일시',
+    updated_at  datetime            not null default current_timestamp comment '데이터 수정일시',
+    primary key (id)
 ) comment '회원';
 
-/* 22.08.16 토큰 테이블 */
+/* 22.08.17 토큰 테이블 */
 CREATE TABLE token
 (
     member_id                         int      not null comment '회원 구분자(seq)',
@@ -26,7 +26,7 @@ CREATE TABLE token
     social_refresh_token_expired_date datetime comment 'Social에서 발급 받은 리프레쉬 토큰 만료일시',
     created_at                        datetime not null default current_timestamp comment '데이터 생성일시',
     updated_at                        datetime not null default current_timestamp comment '데이터 수정일시',
-    FOREIGN KEY (member_id) REFERENCES member (member_id)
+    FOREIGN KEY (member_id) REFERENCES member (id)
 ) comment '토큰';
 
 
