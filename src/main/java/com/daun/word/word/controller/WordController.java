32bd1b9fa50b975d157f0b58bbe1b0af06bc925f
mynameisdaun <daun9870.jung@gmail.com>
2022-08-19 01:00:17 +0900

    <fix>(word-mapper.xml, WordController) 버그 수정

--- src/main/java/com/daun/word/word/controller/WordController.java
+++ src/main/java/com/daun/word/word/controller/WordController.java
@@ -1,14 +1,13 @@
 package com.daun.word.word.controller;
 
-import com.daun.word.word.domain.Word;
 import com.daun.word.word.dto.WordSaveRequest;
 import com.daun.word.word.service.WordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,6 +16,7 @@ public class WordController {
 
     private final WordService wordService;
 
+    @PostMapping("")
     public ResponseEntity<?> save(@RequestBody WordSaveRequest wordSaveRequest) {
         return ResponseEntity.status(201).body(wordService.save(wordSaveRequest));
     }
