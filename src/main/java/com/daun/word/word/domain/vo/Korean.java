fd9ec05570a2cd31c67b8055e5d82cd1e6e3d3ee
mynameisdaun <daun9870.jung@gmail.com>
2022-08-27 18:14:25 +0900

    한국어 표기에 영어나, 특수 기호가 포함 될 수 있음

--- src/main/java/com/daun/word/word/domain/vo/Korean.java
+++ src/main/java/com/daun/word/word/domain/vo/Korean.java
@@ -19,7 +19,8 @@ public class Korean {
         if (isNullOrBlank(korean)) {
             throw new IllegalArgumentException("단어의 한글 표기는 한글자 이상이어야 합니다.");
         }
-        if (!Pattern.matches("^[ㄱ-ㅎ가-힣]*$", korean)) {
+
+        if (!Pattern.matches("^[a-zA-Zㄱ-ㅎ가-힣-_\\s\\.]+$", korean)) {
             throw new IllegalArgumentException("한국어 표기는 한글만 포함하고 있어야 합니다.");
         }
         this.korean = korean;
--- src/test/java/com/daun/word/word/domain/vo/KoreanTest.java
+++ src/test/java/com/daun/word/word/domain/vo/KoreanTest.java
@@ -1,7 +1,6 @@
 package com.daun.word.word.domain.vo;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,25 +13,36 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 class KoreanTest {
 
+    private static Stream<Arguments> valid() {
+        return Stream.of(
+                Arguments.of("할 수 있는"),
+                Arguments.of("- 하다 할 수 있는"),
+                Arguments.of("단어"),
+                Arguments.of("english"),
+                Arguments.of("...에 반대하여")
+
+        );
+    }
+
     private static Stream<Arguments> invalid() {
         return Stream.of(
                 Arguments.of(" "),
                 Arguments.of(""),
                 Arguments.of("한국말1"),
-                Arguments.of("english"),
                 Arguments.of("あいうえお")
         );
     }
 
     @DisplayName(value = "한국어 표기를 생성할 수 있다")
-    @Test
-    void create() throws Exception {
+    @MethodSource("valid")
+    @ParameterizedTest
+    void create(String str) throws Exception {
         //given&&when
-        Korean korean = new Korean("단어");
+        Korean korean = new Korean(str);
         //then
         assertThat(korean).isNotNull();
         assertAll(
-                () -> assertThat(korean.getValue()).isEqualTo("단어")
+                () -> assertThat(korean.getValue()).isEqualTo(str)
         );
     }
 
