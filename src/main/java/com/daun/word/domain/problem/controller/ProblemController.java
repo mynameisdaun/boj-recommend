4fa425f02d286d345ada13171ff78ea3c0bc35b9
mynameisdaun <daun9870.jung@gmail.com>
2022-11-15 12:26:52 +0900

    <feat>(ProblemController) change ResponseEntity body type to GlobalApiResponseDTO

--- src/main/java/com/daun/word/domain/problem/controller/ProblemController.java
+++ src/main/java/com/daun/word/domain/problem/controller/ProblemController.java
@@ -1,6 +1,7 @@
 package com.daun.word.domain.problem.controller;
 
 import com.daun.word.domain.problem.service.ProblemService;
+import com.daun.word.global.dto.ApiResponse;
 import com.daun.word.global.vo.Tier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +15,17 @@ public class ProblemController {
     private final ProblemService problemService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Integer id) {
-        return ResponseEntity.status(200).body(problemService.findById(id));
+    public ResponseEntity<ApiResponse> findById(@PathVariable Integer id) {
+        return ResponseEntity.status(200).body(new ApiResponse(problemService.findById(id)));
     }
 
     @PostMapping("/{id}")
-    public ResponseEntity<?> save(@PathVariable Integer id) {
-        return ResponseEntity.status(200).body(problemService.manual(id));
+    public ResponseEntity<ApiResponse> save(@PathVariable Integer id) {
+        return ResponseEntity.status(200).body(new ApiResponse(problemService.manual(id)));
     }
 
     @GetMapping("/between")
-    public ResponseEntity<?> save(@RequestParam Tier min, @RequestParam Tier max) {
-        return ResponseEntity.status(200).body(problemService.findAllByTierBetween(min, max));
+    public ResponseEntity<ApiResponse> save(@RequestParam Tier min, @RequestParam Tier max) {
+        return ResponseEntity.status(200).body(new ApiResponse(problemService.findAllByTierBetween(min, max)));
     }
 }
