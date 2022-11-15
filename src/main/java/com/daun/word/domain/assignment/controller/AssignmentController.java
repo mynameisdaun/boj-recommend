1af13b0874c8e1f85e201dfd740ac684bad1d3ea
mynameisdaun <daun9870.jung@gmail.com>
2022-11-15 12:24:35 +0900

    <feat>(AssignmentController) change ResponseEntity body type to GlobalApiResponseDTO

--- src/main/java/com/daun/word/domain/assignment/controller/AssignmentController.java
+++ src/main/java/com/daun/word/domain/assignment/controller/AssignmentController.java
@@ -3,6 +3,7 @@ package com.daun.word.domain.assignment.controller;
 import com.daun.word.domain.assignment.dto.AssignRequest;
 import com.daun.word.domain.assignment.dto.StudyAssignRequest;
 import com.daun.word.domain.assignment.service.AssignmentService;
+import com.daun.word.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,24 +20,24 @@ public class AssignmentController {
     private final AssignmentService assignmentService;
 
     @PostMapping("")
-    public ResponseEntity<?> assign(@Valid @RequestBody AssignRequest request) {
-        return ResponseEntity.status(201).body(assignmentService.assign(request));
+    public ResponseEntity<ApiResponse> assign(@Valid @RequestBody AssignRequest request) {
+        return ResponseEntity.status(201).body(new ApiResponse(assignmentService.assign(request)));
     }
 
     @GetMapping("")
-    public ResponseEntity<?> findAll() {
+    public ResponseEntity<ApiResponse> findAll() {
         return ResponseEntity.status(200)
-                .body(assignmentService.findAll());
+                .body(new ApiResponse(assignmentService.findAll()));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable UUID id) {
+    public ResponseEntity<ApiResponse> findById(@PathVariable UUID id) {
         return ResponseEntity.status(200)
-                .body(assignmentService.findById(id));
+                .body(new ApiResponse(assignmentService.findById(id)));
     }
 
     @PostMapping("/study")
-    public ResponseEntity<?> studyAssign(@Valid @RequestBody StudyAssignRequest request) throws AuthenticationException {
-        return ResponseEntity.status(201).body(assignmentService.assignForStudy(request));
+    public ResponseEntity<ApiResponse> studyAssign(@Valid @RequestBody StudyAssignRequest request) throws AuthenticationException {
+        return ResponseEntity.status(201).body(new ApiResponse(assignmentService.assignForStudy(request)));
     }
 }
