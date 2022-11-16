97f6408d6c953369a925883bc66eec0a66eecaa5
mynameisdaun <daun9870.jung@gmail.com>
2022-11-16 16:39:44 +0900

    <data>(import.sql) add more problem init data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,24 +6,43 @@ insert into member (member_id,email,last_login_at,name,password,social_type,leve
 insert into member (member_id,email,last_login_at,name,password,social_type,level,rate) values (x'4721ee722ff3417fade3acd0a804605b','baggomsoon96',sysdate(),'baggomsoon','$2a$10$j.X5k/3SVnZI/VxSFkjw..n2cc5auOyWYp2z.kksSU0iYCgHcwfyS','W',13,'g3');
 
 /* tag */
+insert into tag(tag_id, tag_key, title) values (7, 'graphs','그래프 이론');
+insert into tag(tag_id, tag_key, title) values (11, 'graph_traversal','그래프 탐색');
+insert into tag(tag_id, tag_key, title) values (25, 'dp','다이나믹 프로그래밍');
 insert into tag(tag_id, tag_key, title) values (33, 'greedy','그리디 알고리즘');
+insert into tag(tag_id, tag_key, title) values (49, 'mst','최소 스패닝 트리');
 insert into tag(tag_id, tag_key, title) values (71, 'stack','스택');
-insert into tag(tag_id, tag_key, title) values (158, 'string','문자열');
-insert into tag(tag_id, tag_key, title) values (175, 'data_structures','자료 구조');
+insert into tag(tag_id, tag_key, title) values (97, 'sorting','정렬');
 insert into tag(tag_id, tag_key, title) values (100, 'geometry','기하학');
 insert into tag(tag_id, tag_key, title) values (124, 'math','수학');
+insert into tag(tag_id, tag_key, title) values (126, 'bfs','너비 우선 탐색');
+insert into tag(tag_id, tag_key, title) values (158, 'string','문자열');
+insert into tag(tag_id, tag_key, title) values (175, 'data_structures','자료 구조');
+
 
 /* problem */
-insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(16120, 12, 'g4', 'PPAP', 'https://www.acmicpc.net/problem/16120', 1379);
 insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(1002, 8, 's3', '터렛', 'https://www.acmicpc.net/problem/1002', 29321);
+insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(2011, 11, 'g5', '암호코드', 'https://www.acmicpc.net/problem/2011', 6002);
+insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(16120, 12, 'g4', 'PPAP', 'https://www.acmicpc.net/problem/16120', 1379);
+insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(2245, 15, 'g1', '배열 정리하기', 'https://www.acmicpc.net/problem/2245', 17);
+insert into problem (problem_id, level, rate, title, url,accepted_user_count) values(2887, 16, 'p5', '행성 터널', 'https://www.acmicpc.net/problem/2887', 4643);
+
 
 /* problem tag */
-insert into problem_tag(id,problem_id, tag_id) values (300000,16120,33);
-insert into problem_tag(id,problem_id, tag_id) values (300001,16120,71);
-insert into problem_tag(id,problem_id, tag_id) values (300002,16120,158);
-insert into problem_tag(id,problem_id, tag_id) values (300003,16120,175);
-insert into problem_tag(id,problem_id, tag_id) values (300004,1002,100);
-insert into problem_tag(id,problem_id, tag_id) values (300005,1002,124);
+insert into problem_tag(id, problem_id, tag_id) values (300000,16120,33);
+insert into problem_tag(id, problem_id, tag_id) values (300001,16120,71);
+insert into problem_tag(id, problem_id, tag_id) values (300002,16120,158);
+insert into problem_tag(id, problem_id, tag_id) values (300003,16120,175);
+insert into problem_tag(id, problem_id, tag_id) values (300004,1002,100);
+insert into problem_tag(id, problem_id, tag_id) values (300005,1002,124);
+insert into problem_tag(id, problem_id, tag_id) values (300007,2245,7);
+insert into problem_tag(id, problem_id, tag_id) values (300006,2245,11);
+insert into problem_tag(id, problem_id, tag_id) values (300008,2245,126);
+insert into problem_tag(id, problem_id, tag_id) values (300009,2011,25);
+insert into problem_tag(id, problem_id, tag_id) values (300010,2887,7);
+insert into problem_tag(id, problem_id, tag_id) values (300011,2887,49);
+insert into problem_tag(id, problem_id, tag_id) values (300012,2887,97);
+
 
 /* study and study member */
 insert into study(study_id,hash,name,member_id) values (x'1ee72417fea22ff34a80dac3d075b046','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','example_study',x'f1860abc2ea1411bbd4abaa44f0d5580');
