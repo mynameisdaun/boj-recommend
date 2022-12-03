a1577d260d6f2c2496df704574140d367982d5cd
mynameisdaun <daun9870.jung@gmail.com>
2022-12-03 16:02:08 +0900

    <test> RecommendRepositoryTest 추천을 저장한다

--- src/test/java/com/daun/word/domain/recommend/domain/repository/RecommendRepositoryTest.java
+++ src/test/java/com/daun/word/domain/recommend/domain/repository/RecommendRepositoryTest.java
@@ -1,14 +1,17 @@
 package com.daun.word.domain.recommend.domain.repository;
 
+import com.daun.word.Fixture.Fixture;
 import com.daun.word.domain.recommend.domain.Recommend;
+import com.daun.word.domain.recommend.domain.vo.RecommendType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
-import static com.daun.word.Fixture.Fixture.recommend;
+import static com.daun.word.Fixture.Fixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -39,11 +42,18 @@ class RecommendRepositoryTest {
     @Test
     void save() throws Exception {
         //given
-
+        UUID id = UUID.randomUUID();
+        Recommend r = new Recommend(id, problem_16120(), daun9870jung(), RecommendType.TIER);
         //when
-
+        Recommend save = recommendRepository.save(r);
         //then
-
+        assertThat(save).isNotNull();
+        assertAll(
+                () -> assertThat(save.getId()).isEqualTo(id),
+                () -> assertThat(save.getMember().getId()).isEqualTo(daun9870jung().getId()),
+                () -> assertThat(save.getType()).isEqualTo(RecommendType.TIER),
+                () -> assertThat(save.getProblem().getId()).isEqualTo(problem_16120().getId())
+        );
     }
 
 }
